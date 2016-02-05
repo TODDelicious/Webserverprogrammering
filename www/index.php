3e1a6db6762efc54dab4bb744152172305c4f126
TODDelicious <todd.alves@hotmail.com>
2016-02-05 15:08:56 +0100

    Uppgift 7a med städer

--- www/index.php
+++ www/index.php
@@ -1,4 +1,19 @@
 <?php
+	require_once('../mysql_connect.php');
+	//Gör en anslutning till batabasen
 
+	$query = 'SELECT * FROM Cities WHERE Population>=5000000 ORDER BY Population DESC;';
+	//Selectar datdan från tabellen i mysql servern
 
+	$result = mysqli_query($dbc, $query);
+
+	echo '<table border="solid">';
+	//Lägger in så att tabellen får kanetr mellan alla celler
+	while($row = mysqli_fetch_array($result)){
+		echo '<tr><td>' . ucfirst($row['City']) . ' </td> <td> ' . $row['Population'] . '</td></tr>';
+	}
+	//En loop som skriver ut städer i storleksårdning från störst (med över 5 000 000 invånare)
+
+	echo '</table>';
+	
 ?>
