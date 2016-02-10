c84c8814e0b6ac95ec9af62587480165e1cd9b9b
TODDelicious <todd.alves@hotmail.com>
2016-02-10 13:38:27 +0100

    All skit + det jag har gjort

--- mysql_connect.php
+++ mysql_connect.php
@@ -1,9 +1,9 @@
-+<?php
+<?php
  
  	DEFINE('DB_USER','root');
  	DEFINE('DB_HOST','localhost');
  	DEFINE('DB_PASSWORD','camecase1234');
- 	DEFINE('DB_DATABASE','intro');
+ 	DEFINE('DB_DATABASE','formulär');
  		
  		
  	$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
---
+++ www/Formular/formularet.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+
+		<link rel="Stylesheet" href="formularet.css" >
+
+	</head>
+	
+	<body>
+		
+		<p id="rubrik"> Formulär </p>
+		
+		<main>
+		
+			<table>
+			
+				<form action="skicka/index.php" method="post">
+				
+					<tr>
+						<td>
+							<p>Namn:</p><p class="color-red">*</p><br>
+							<input type="text" name="Anmälare" required>
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+							<p>Mail:</p><p class="color-red">*</p><br>
+							<input type="text" name="Mail" size="30" required>
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+							<p>Kort beskrivning:</p><p class="color-red">*</p><br>
+							<input type="text" name="Kort_beskrivning" size="40" required>
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+							<p>Detaljerad beskrivning:</p><br>
+							<textarea type="text" name="Detaljerad_beskrivning" rows="5" cols="40"></textarea>
+						</td>
+					</tr>
+					
+					<tr>
+						<td>
+							<p> Prioritet</p><p class="color-red">*</p><br>
+							<input type="radio" name="Prioritet" value="1" required> Mycket H�g
+							<input type="radio" name="Prioritet" value="2"> Hög
+							<input type="radio" name="Prioritet" value="3" checked> Medel  
+							<input type="radio" name="Prioritet" value="4"> Låg 
+						</td>
+					</tr>
+					
+					<tr>
+						<td class="padding-0">
+							<p class="color-red font-size-8 float-right"> * = Obligatoriskt </p>
+						</td>
+					</tr>
+					
+					<tr>
+						<td class="center">
+							<input type="submit" name="Submit" value="Skicka">
+						</td>
+					</tr>
+					
+				
+				</form>
+				
+			</table>
+			
+		</main>
+	
+	</body>
+	
+</html>
